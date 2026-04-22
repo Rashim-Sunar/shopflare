15a2c6b50dc1d1c5227c4eb67bbc87b2ea74bde3
Rashim-Sunar <rashimsunar.02@gmail.com>
2026-04-22 12:09:47 +0530

    feat(frontend): migrate frontend codebase to TypeScript

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import { Toaster } from 'react-hot-toast';
 
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+createRoot(rootElement).render(
   <StrictMode>
      <Toaster position="top-center" reverseOrder={false} />
      <Provider store={store}>
